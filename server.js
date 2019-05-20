2b163930940beb41ec07151dac5e3add03408851
maors1 <sap.sapir@hotmail.com>
2019-05-20 16:55:21 +0300

    Add functions

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index')
 var tasks = require('./routes/tasks')
 
@@ -28,5 +27,5 @@ app.use('/', index);
 app.use('/api',tasks);
 
 app.listen(port, function(){
-    console.log('Server started on port: '+port);
-});
+    console.log('Server started on port: ' + port);
+    });
